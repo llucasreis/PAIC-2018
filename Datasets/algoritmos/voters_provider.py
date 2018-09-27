f0c9fa29a9638c635d455c70fb76f6d2e394e136
Lucas Pereira Reis <lprs.eng@uea.edu.br>
2018-09-27 19:08:08 -0400

    feat: implementation of a voter's provider
    To create the second dataset, instead of count words from candidate's proposal, this time the data will be voter's rating about some candidate. The rating is calculated by giving rating to some areas and then pick up the mean or median. This code was created to optimize this step by creating it automatically.

---
+++ Datasets/algoritmos/voters_provider.py
@@ -0,0 +1,31 @@
+import random
+
+rating = [1,2,3,4,5]
+candidates = ["alckmin","alvaro","amoedo","bolsonaro","boulos","ciro","daciolo","goulart","haddad","marina","meirelles"]
+
+maxOfVoters = 10
+voters = {}
+
+for i in range(1,maxOfVoters):
+    numberOfCandidates = random.randint(3, len(candidates))
+    
+    aux = {}
+    for _ in range(numberOfCandidates):
+        candidate = candidates[random.randint(0, len(candidates) - 1)]
+        
+        while candidate in aux.keys():
+            candidate = candidates[random.randint(0, len(candidates) - 1)]
+
+        aux[candidate] = {
+            'saude' : random.choice(rating),
+            'seguranca' : random.choice(rating),
+            'educacao' : random.choice(rating),
+            'economia' : random.choice(rating),
+            'cultura' : random.choice(rating),
+            'tecnologia': random.choice(rating),
+            'meio ambiente': random.choice(rating)
+        }
+    voters["voter" + str(i)] = aux
+
+def getVoters():
+	return voters
