444c8ed485ce78b24516df0ab9722dcba818fd6f
Lucas Pereira Reis <lprs.eng@uea.edu.br>
2018-12-04 20:57:03 -0400

    chore: change code's name
    Also add rows's name

---
+++ Desenvolvimento/recomendação/content_based_recommendation.py
@@ -0,0 +1,46 @@
+from math import *
+import numpy as np
+import pandas as pd
+
+
+df = pd.read_csv('../datasets/candidate.csv')
+candidates = ['Jair Bolsonaro', 'Ciro Gomes', 'Cabo Daciolo', 'Guilherme Boulos', 'Fernando Haddad', 'Marina Silva']
+df.index = candidates
+
+a = 1
+b = 5
+
+min_, max_ = min(df.min()), max(df.max())
+
+def normalize(x):
+	return ((b - a) * ((x - min_) / (max_ - min_))) + a
+
+df = normalize(df)
+
+dict_cand = df.T.to_dict()
+
+
+def minkowski(rating1, rating2, r=1):
+	distance = 0
+	commonRatings = False
+
+	for key in rating1:
+		if key in rating2:
+			distance += pow(abs(rating1[key] - rating2[key]), r)
+			commonRatings = True
+
+	if commonRatings:
+		return pow(distance, 1/r)
+	else:
+		return float('Inf')
+
+def recommend(userRating):
+	distances = []
+
+	for candidate in dict_cand:
+
+		distance = minkowski(dict_cand[candidate], userRating)
+		distances.append((candidate, distance))
+
+	return sorted(distances,
+		key=lambda candidateTuple: candidateTuple[1])
